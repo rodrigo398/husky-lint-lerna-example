d1baf8166740e0e93dd021354faa12f1ba48b418
Rodrigo Avila <rodrigo398@gmail.com>
2021-07-31 18:52:52 +1000

    chore: rename precommit command

--- packages/project1/src/App.tsx
+++ packages/project1/src/App.tsx
@@ -1,8 +1,7 @@
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  console.log('test here')
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
