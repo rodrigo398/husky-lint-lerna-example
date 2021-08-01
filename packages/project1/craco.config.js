85142874a1e6044e90b108f9420ee47da838ebbd
Rodrigo Avila <rodrigo398@gmail.com>
2021-08-01 11:23:06 +1000

    chore: add comments about craco build

--- packages/project1/craco.config.js
+++ packages/project1/craco.config.js
@@ -15,10 +15,10 @@ module.exports = {
     // inject react imports instead
     // This workaround: https://github.com/pradel/create-react-app-esbuild/issues/7
     // Main esbuild issue: https://github.com/evanw/esbuild/issues/334
-    plugins: [new ProvidePlugin({ React: 'react' })],
+    plugins: [new ProvidePlugin({ React: 'react' })], // Doing this will still rely on webpack for dependency injection, which means that even though the build process is much faster now it'll be slightly slower than the potential.
   },
   typescript: {
-    enableTypeChecking: true,
+    enableTypeChecking: !process.env.TRANSPILE_ONLY, // Craco turns on type checking by default. We want to typecheck during development and on build, however, to keep builds fast in CI we want to turn off type checks on build:ci
   },
   // Esbuild settings
   plugins: [
